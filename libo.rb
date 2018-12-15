27c24f455dcf0f888c304a2828638fc2f1527f90
Hodaka Suzuki <altitude3190@gmail.com>
2018-12-15 18:43:47 +0900

    update libo.rb

--- libo.rb
+++ libo.rb
@@ -1,34 +1,11 @@
-# Documentation: https://docs.brew.sh/Formula-Cookbook
-#                https://www.rubydoc.info/github/Homebrew/brew/master/Formula
-# PLEASE REMOVE ALL GENERATED COMMENTS BEFORE SUBMITTING YOUR PULL REQUEST!
 class Libo < Formula
   desc "Easy access to LICENSE boilerplates"
   homepage "https://github.com/altitude3190/libo"
   url "https://github.com/altitude3190/libo/archive/v1.0.0.tar.gz"
   sha256 "603cd45d1dae9a95f68a530db7319190a6e115fece70262434e0940c2f1df7d9"
-  # depends_on "cmake" => :build
 
   def install
-    # ENV.deparallelize  # if your formula fails when building in parallel
-    # Remove unrecognized options if warned by configure
-    system "./configure", "--disable-debug",
-                          "--disable-dependency-tracking",
-                          "--disable-silent-rules",
-                          "--prefix=#{prefix}"
-    # system "cmake", ".", *std_cmake_args
-    system "make", "install" # if this fails, try separate make/make install steps
+    bin.install "libo"
   end
 
-  test do
-    # `test do` will create, run in and delete a temporary directory.
-    #
-    # This test will fail and we won't accept that! For Homebrew/homebrew-core
-    # this will need to be a test that verifies the functionality of the
-    # software. Run the test with `brew test libo`. Options passed
-    # to `brew install` such as `--HEAD` also need to be provided to `brew test`.
-    #
-    # The installed folder is not in the path, so use the entire path to any
-    # executables being tested: `system "#{bin}/program", "do", "something"`.
-    system "false"
-  end
 end
