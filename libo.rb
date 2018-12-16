1a76f95001eb98511bf8d0f5fd54b8901f7e11cc
Hodaka Suzuki <altitude3190@gmail.com>
2018-12-17 02:22:57 +0900

    update checksum

--- libo.rb
+++ libo.rb
@@ -2,7 +2,7 @@ class Libo < Formula
   desc "Easy access to LICENSE boilerplates"
   homepage "https://github.com/altitude3190/libo"
   url "https://github.com/altitude3190/libo/archive/v1.0.0.tar.gz"
-  sha256 "603cd45d1dae9a95f68a530db7319190a6e115fece70262434e0940c2f1df7d9"
+  sha256 "72921ae9821e91cc83fb592cdf25dfed93d58b09adfcb276c77bd023ff16ed11"
 
   def install
     prefix.install Dir["boilerplates/"], "libo.pl"
