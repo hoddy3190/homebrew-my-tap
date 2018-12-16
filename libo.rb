b852854c8d240c3ff45e08f4297d71568ea91f8a
Hodaka Suzuki <altitude3190@gmail.com>
2018-12-17 01:28:38 +0900

    update how to install

--- libo.rb
+++ libo.rb
@@ -5,7 +5,8 @@ class Libo < Formula
   sha256 "603cd45d1dae9a95f68a530db7319190a6e115fece70262434e0940c2f1df7d9"
 
   def install
-    bin.install "libo.pl"
+    prefix.install Dir["boilerplates/"], "libo.pl"
+    bin.install_symlink("../libo.pl" => "libo")
   end
 
 end
