b3adabaabe5bfe09dd258e81f239ca73cc6a7edb
Hodaka Suzuki <altitude3190@gmail.com>
2019-05-14 00:18:23 +0900

    Add Casks

---
+++ Casks/hodito.rb
@@ -0,0 +1,10 @@
+cask 'hodito' do
+  version '0.1.0'
+  sha256 'b7a18154edba800017cd735dda6b86606de1c99bb4ed8a9fe03ff14eb1cfb5c4'
+
+  url "https://github.com/hoddy3190/Hodito/releases/download/v0.1.0-beta.1/Hodito.app.zip"
+  name 'Hodito'
+  homepage 'https://github.com/hoddy3190/Hodito'
+
+  app 'Hodito.app'
+end
