3488cf8ba7f75a7b53bce382dc4d0fe3735e6829
Hodaka Suzuki <altitude3190@gmail.com>
2020-01-10 00:40:51 +0900

    Create alloy.rb

---
+++ Casks/alloy.rb
@@ -0,0 +1,10 @@
+cask 'alloy' do
+  version '5.1.0'
+  sha256 :no_check
+
+  url "https://github.com/AlloyTools/org.alloytools.alloy/releases/download/v5.1.0/alloy.dmg"
+  name 'alloy'
+  homepage 'https://github.com/AlloyTools/org.alloytools.alloy'
+
+  app 'Alloy'
+end
