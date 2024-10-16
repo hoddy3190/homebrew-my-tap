0546718f5f6bb4b4c4fbe5999e0f2a260f2953d4
Hodaka Suzuki <altitude3190@gmail.com>
2024-10-16 23:20:11 +0900

    feat: lgbd project_build dir

--- lgbd.rb
+++ lgbd.rb
@@ -2,7 +2,7 @@ class Lgbd < Formula
   head "https://github.com/hoddy3190/anylang-build", :using => :git
 
   def install
-    prefix.install Dir["envs/"], "lgbd.sh"
+    prefix.install Dir["envs/, project_build/"], "lgbd.sh"
     bin.install_symlink("../lgbd.sh" => "lgbd")
   end
 end
