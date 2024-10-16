a8b2c3d75b7ff1bad5623895b7b2883e638e4573
Hodaka Suzuki <altitude3190@gmail.com>
2024-10-16 23:24:44 +0900

    fix: branch main

--- lgbd.rb
+++ lgbd.rb
@@ -1,8 +1,8 @@
 class Lgbd < Formula
-  head "https://github.com/hoddy3190/anylang-build", :using => :git
+  head "https://github.com/hoddy3190/anylang-build", branch: "main", using: :git
 
   def install
-    prefix.install Dir["envs/, project_build/"], "lgbd.sh"
+    prefix.install Dir["envs/", "project_build/"], "lgbd.sh"
     bin.install_symlink("../lgbd.sh" => "lgbd")
   end
 end
